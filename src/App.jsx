33a88f733726bdf0b59317d3ecf9ed2e7f08de40
Badsha Hossain <badshab454@gmail.com>
2023-05-19 21:41:19 +0600

    hello world

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>hello world</h2>
+      <h1>this one bigger</h1>
+    </>
+  );
+}
+
+export default App;
