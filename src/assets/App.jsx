097c3b19b08b0ce79d857dec8e78bf503ded1a2b
Badsha Hossain <badshab454@gmail.com>
2023-05-20 00:59:02 +0600

    nav bar

---
+++ src/assets/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <img src="/img/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </>
+  );
+};
+
+export default App;
