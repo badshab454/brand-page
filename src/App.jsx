503252fba8b99b0bba90c54933cc1c5728fddc89
Badsha Hossain <badshab454@gmail.com>
2023-05-20 13:34:10 +0600

    navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import "./App.css";
+import Navigation from "./components/Navigation.jsx";
 
 const App = () => {
-  return (
-    <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <img src="/img/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">MENU</li>
-          <li href="#">LOCATION</li>
-          <li href="#">ABOUT</li>
-          <li href="#">CONTACT</li>
-        </ul>
-        <button>Login</button>
-      </nav>
-    </>
-  );
+  return <Navigation />;
 };
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <>
+      <nav className="nav-bar">
+        <div className="logo">
+          <img src="/img/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
