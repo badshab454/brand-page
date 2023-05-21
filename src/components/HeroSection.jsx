a3fed5a2f90a56faf7bd36dfb51813de4a47f2e6
Badsha Hossain <badshab454@gmail.com>
2023-05-21 23:43:32 +0600

    hero content styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
   return (
-    <>
-      <div className="hero-content">
+    <div className="hero-section">
+      <div className="hero-conetnt">
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
           YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
@@ -12,9 +12,16 @@ const HeroSection = () => {
           <button>Shop Now</button>
           <button>Category</button>
         </div>
+        <p>Also Available On</p>
+        <div className="shopping-section">
+          <img src="/img/flipkart.png" alt="flipkart" />
+          <img src="/img/amazon.png" alt="amazon" />
+        </div>
       </div>
-      <div className="hero-img"></div>
-    </>
+      <div className="hero-img">
+        <img src="/img/shoe_image.png" alt="nike-shoe" />
+      </div>
+    </div>
   );
 };
 
